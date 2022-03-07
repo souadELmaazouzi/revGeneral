9b6117b6167326d36b08c480bc13560ca84ca74d
user <user@DESKTOP-INU93R5>
2022-03-07 16:08:14 +0100

    Ajoutez des fichiers projet.

---
+++ revGeneral/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace revGeneral
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SEARCH = new System.Windows.Forms.Button();
+            this.DELETE = new System.Windows.Forms.Button();
+            this.UPDATE = new System.Windows.Forms.Button();
+            this.ADD = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(311, 284);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(102, 33);
+            this.button8.TabIndex = 41;
+            this.button8.Text = ">>";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(217, 284);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(102, 33);
+            this.button7.TabIndex = 40;
+            this.button7.Text = ">";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(117, 284);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(102, 33);
+            this.button6.TabIndex = 39;
+            this.button6.Text = "<";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(24, 284);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 33);
+            this.button5.TabIndex = 38;
+            this.button5.Text = "<<";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // SEARCH
+            // 
+            this.SEARCH.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SEARCH.Location = new System.Drawing.Point(170, 395);
+            this.SEARCH.Name = "SEARCH";
+            this.SEARCH.Size = new System.Drawing.Size(114, 43);
+            this.SEARCH.TabIndex = 37;
+            this.SEARCH.Text = "SEARCH";
+            this.SEARCH.UseVisualStyleBackColor = true;
+            this.SEARCH.Click += new System.EventHandler(this.SEARCH_Click);
+            // 
+            // DELETE
+            // 
+            this.DELETE.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DELETE.Location = new System.Drawing.Point(170, 336);
+            this.DELETE.Name = "DELETE";
+            this.DELETE.Size = new System.Drawing.Size(114, 43);
+            this.DELETE.TabIndex = 36;
+            this.DELETE.Text = "DELETE";
+            this.DELETE.UseVisualStyleBackColor = true;
+            this.DELETE.Click += new System.EventHandler(this.DELETE_Click);
+            // 
+            // UPDATE
+            // 
+            this.UPDATE.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UPDATE.Location = new System.Drawing.Point(24, 395);
+            this.UPDATE.Name = "UPDATE";
+            this.UPDATE.Size = new System.Drawing.Size(130, 43);
+            this.UPDATE.TabIndex = 35;
+            this.UPDATE.Text = "UPDATE";
+            this.UPDATE.UseVisualStyleBackColor = true;
+            this.UPDATE.Click += new System.EventHandler(this.UPDATE_Click);
+            // 
+            // ADD
+            // 
+            this.ADD.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ADD.Location = new System.Drawing.Point(24, 336);
+            this.ADD.Name = "ADD";
+            this.ADD.Size = new System.Drawing.Size(130, 43);
+            this.ADD.TabIndex = 34;
+            this.ADD.Text = "ADD";
+            this.ADD.UseVisualStyleBackColor = true;
+            this.ADD.Click += new System.EventHandler(this.ADD_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(353, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(435, 212);
+            this.dataGridView1.TabIndex = 33;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(150, 237);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 21);
+            this.comboBox1.TabIndex = 32;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(142, 198);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(181, 20);
+            this.textBox5.TabIndex = 31;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(142, 164);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(181, 20);
+            this.textBox4.TabIndex = 30;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 126);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 20);
+            this.textBox3.TabIndex = 29;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 83);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.TabIndex = 28;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.TabIndex = 27;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 20);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "ENTREPRISE :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(28, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 20);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "SALAIRE :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(28, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "AGE :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "PRENOM :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "NOM :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "ID :";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(423, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 47);
+            this.button1.TabIndex = 42;
+            this.button1.Text = " employe order by entreprise";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.SEARCH);
+            this.Controls.Add(this.DELETE);
+            this.Controls.Add(this.UPDATE);
+            this.Controls.Add(this.ADD);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button SEARCH;
+        private System.Windows.Forms.Button DELETE;
+        private System.Windows.Forms.Button UPDATE;
+        private System.Windows.Forms.Button ADD;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ revGeneral/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace revGeneral
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        revGeneralEntities db = new revGeneralEntities();
+        BindingSource bs = new BindingSource();
+        private void afficher()
+        {
+            var req = from emp in db.employes
+                      select new
+                      {
+                          emp.id,
+                          emp.nom,
+                          emp.prenom,
+                          emp.age,
+                          emp.salaire,
+                          emp.entreprise.name
+                      };
+            bs.DataSource = req.ToList();
+            dataGridView1.DataSource = bs;
+            textBox1.DataBindings.Add("Text", bs, "id");
+            textBox2.DataBindings.Add("Text", bs, "nom");
+            textBox3.DataBindings.Add("Text", bs, "prenom");
+            textBox4.DataBindings.Add("Text", bs, "age");
+            textBox5.DataBindings.Add("Text", bs, "salaire");
+            comboBox1.DataBindings.Add("Text", bs, "name");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            afficher();
+            comboBox1.DataSource = db.entreprises.ToList();
+            comboBox1.DisplayMember = "name";
+            comboBox1.ValueMember = "code";
+        }
+
+        private void ADD_Click(object sender, EventArgs e)
+        {
+            employe e1 = new employe();
+            e1.id = int.Parse(textBox1.Text);
+            e1.nom = textBox2.Text;
+            e1.prenom = textBox3.Text;
+            e1.age = int.Parse(textBox4.Text);
+            e1.salaire = float.Parse(textBox5.Text);
+            e1.code = int.Parse(comboBox1.SelectedValue.ToString());
+            db.employes.Add(e1);
+            db.SaveChanges();
+            MessageBox.Show("bien ajouter ");
+        }
+
+        private void UPDATE_Click(object sender, EventArgs e)
+        {
+            employe e1 = new employe();
+            e1.id = int.Parse(textBox1.Text);
+            employe e2 = new employe();
+            e2 = (from emp in db.employes
+                  where emp.id == e1.id
+                  select emp).FirstOrDefault();
+            e2.nom = textBox2.Text;
+            e2.prenom = textBox3.Text;
+            e2.age = int.Parse(textBox4.Text);
+            e2.salaire = float.Parse(textBox5.Text);
+            e2.code = int.Parse(comboBox1.SelectedValue.ToString());
+            db.SaveChanges();
+        }
+
+        private void SEARCH_Click(object sender, EventArgs e)
+        {
+            employe e1 = new employe();
+            e1.id = int.Parse(textBox1.Text);
+            employe e2 = new employe();
+             e2 = (from emp in db.employes
+                     where emp.id == e1.id
+                     select emp).FirstOrDefault();
+            textBox2.Text = e2.nom;
+            textBox3.Text = e2.prenom;
+            textBox4.Text = e2.age.ToString();
+            textBox5.Text = e2.salaire.ToString();
+            comboBox1.Text = e2.entreprise.name;
+            
+            
+        }
+
+        private void DELETE_Click(object sender, EventArgs e)
+        {
+            employe em1 = new employe();
+            em1.id = int.Parse(textBox1.Text);
+            employe em = new employe();
+            em = (from emp in db.employes
+                  where emp.id == em1.id
+                  select emp).FirstOrDefault();
+            db.employes.Remove(em);
+            db.SaveChanges();
+            MessageBox.Show("bien supprimer");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            bs.MoveFirst();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bs.MovePrevious();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            bs.MoveNext();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            bs.MoveLast();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var req = from emp in db.employes
+                      where emp.entreprise.name == comboBox1.Text
+                      select new { emp.nom, emp.prenom };
+            dataGridView1.DataSource = req.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
